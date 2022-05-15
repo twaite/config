b29a76ea390c4b02d4928d258ab982ab63b896d5
Tim Waite <tim@dialexa.com>
2022-05-15 03:10:37 -0500

    WIP on setting up new lua config. Plugins don't work when not in config dir :/

--- nvim/keymaps.lua
+++ nvim/keymaps.lua
@@ -9,3 +9,6 @@ keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 
+keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<CR>');
+keymap.set('n', '<leader>r', '<cmd>NvimTreeRefresh<CR>');
+keymap.set('n', '<leader>n', '<cmd>NvimTreeFindFile<CR>');
